ebabaa8e7174ec3da68d747a6a44ae49474429d6
Louis <louis.zk143@gmail.com>
2026-01-05 20:50:19 +0100

    Increase epochs

--- transforms.py
+++ transforms.py
@@ -19,7 +19,6 @@ def get_transforms(mode="weak", is_ms=False):
             raise ValueError("Strong augmentation is disabled for MS data.")
         
         aug_layers = transforms.Compose([
-            transforms.RandomResizedCrop(size=64, scale=(0.75, 1.0), ratio=(0.9, 1.1)),
             transforms.RandomHorizontalFlip(p=0.5),
             transforms.RandomVerticalFlip(p=0.5),
             transforms.ColorJitter(
